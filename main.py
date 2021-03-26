d0dcb2973739d062f91088034aca43fbc4de78a1
alfredez <alfredez008@hotmail.com>
2021-03-26 14:20:23 +0100

    correct

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-#test branch
-import serial
-ser = serial.Serial(
-    port="COM4", baudrate=38400, bytesize=8, stopbits=serial.STOPBITS_ONE
-)
-ser.flushInput()
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-while True:
-    try:
-        ser_bytes = ser.readline()
-        print(ser_bytes)
-    except:
-        print("Keyboard Interrupt")
-        break
+
+class Watcher:
+    DIRECTORY_TO_WATCH = "./correct"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+#and '~' not in event.src_path
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print ("Received modified event - %s." % event.src_path)
+
+        elif event.event_type == 'deleted':
+            # Taken any action here when a file is modified.
+            print("Received deleted event - %s." % event.src_path)
+
+        elif event.event_type == 'closed':
+            # Taken any action here when a file is modified.
+            print("Received closed event - %s." % event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
